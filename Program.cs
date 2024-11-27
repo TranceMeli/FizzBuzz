3dcb4dbe5e18f386e6fc1c9028edd8f3e2d11f60
Schadl <schadl.melanie@training.lug-ag.de>
2024-11-27 22:26:37 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz\n");
+
+            for (int x = 1; x <= 100; x++)
+            {
+                if (x % 3 == 0 && x % 5 == 0)
+                {
+                    Console.WriteLine(x + " FizzBuzz");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.WriteLine(x + " Fizz");
+                }
+                else if (x % 5 == 0 && x % 5 == 0)
+                {
+                    Console.WriteLine(x + " Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
